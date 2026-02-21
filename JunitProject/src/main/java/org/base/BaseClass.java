6ce21dda421c1b5a92da698fd86784eeec5c2d79
AJITH V <113958920+AjithQA-hub@users.noreply.github.com>
2026-02-21 13:18:39 +0530

    Update BaseClass.java

--- JunitProject/src/main/java/org/base/BaseClass.java
+++ JunitProject/src/main/java/org/base/BaseClass.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-//import java.nio.file.FileAlreadyExistsException;
-//import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -130,4 +128,5 @@ public class BaseClass {
 	}
 	}
 }
+
 
